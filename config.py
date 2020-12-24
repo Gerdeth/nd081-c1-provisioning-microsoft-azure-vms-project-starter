8931fab2561d3ec9d7945614e80e9048c4aad197
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2020-12-23 23:11:25 -0500

    client id

--- config.py
+++ config.py
@@ -30,7 +30,8 @@ class Config(object):
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = "37b51057-9385-4904-a3cc-eca671fecf94"
+"
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
